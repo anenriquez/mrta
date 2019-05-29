d7bd07008b6c8f6b04d00d6c9d2924fa86fd8fb5
Angela Enriquez <anenriqu@outlook.com>
2019-05-29 10:28:00 +0200

    Adding setup file

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='mrta_allocation',
+      version='0.1.0',
+      description='Implements the TeSSI algorithm',
+      author='Angela Enriquez Gomez',
+      author_email='angela.enriquez@smail.inf.h-brs.de',
+      package_dir={'': '.'}
+      )
