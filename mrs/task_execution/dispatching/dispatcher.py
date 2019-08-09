9e387f7ecba1f96fe76bc108ba226ea22b548bd3
Angela Enriquez <anenriqu@outlook.com>
2019-08-09 17:04:24 +0200

    Scheduling and dispatching tasks based on the info in the timetable

--- mrs/task_execution/dispatching/dispatcher.py
+++ mrs/task_execution/dispatching/dispatcher.py
@@ -1,3 +1,4 @@
+from ropod.utils.timestamp import TimeStamp as ts
 from mrs.task_execution.dispatching.scheduler import Scheduler
 from mrs.timetable import Timetable
 from stn.stp import STP
@@ -10,24 +11,50 @@ class Dispatcher(object):
         self.ccu_store = ccu_store
         self.task_cls = task_cls
         self.stp = STP(stp_solver)
-
         self.scheduler = Scheduler(robot_id, ccu_store, self.stp)
+
+        self.timetable = self.get_timetable()
 
     def get_timetable(self):
         timetable_dict = self.ccu_store.get_timetable(self.id)
-        print("Timetable dict: ", timetable_dict)
         if timetable_dict is None:
             return
-        timetable = Timetable.from_dict(timetable_dict, self.stp)
-        return timetable
+        self.timetable = Timetable.from_dict(timetable_dict, self.stp)
+        return self.timetable
 
     def run(self):
-        print("Running dispatcher")
-        timetable = self.get_timetable()
-        task_id = timetable.get_earliest_task_id()
+
+        if not self.scheduler.task_scheduled and self.get_timetable():
+            task = self.get_next_task_to_schedule()
+            self.scheduler.schedule_task(task, self.timetable)
+            self.get_timetable()
+
+        if self.scheduler.task_scheduled and self.time_to_dispatch():
+            self.dispatch()
+            self.scheduler.reset_schedule()
+
+    def get_next_task_to_schedule(self):
+        task_id = self.timetable.get_earliest_task_id()
         if task_id:
-            print("------>", task_id)
             task_dict = self.ccu_store.get_task(task_id)
             task = self.task_cls.from_dict(task_dict)
-            print("------>", task)
+            return task
+
+    def time_to_dispatch(self):
+        current_time = ts.get_time_stamp()
+        if current_time < self.scheduler.navigation_start_time:
+            return False
+        return True
+
+    def dispatch(self):
+        current_time = ts.get_time_stamp()
+        print("Dispatching task at: ", current_time)
+
+
+
+
+
+
+
+
 
