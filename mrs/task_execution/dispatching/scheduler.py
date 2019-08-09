99ffa6e557c9915ea01720419e83ce5028b1a8f5
Angela Enriquez <anenriqu@outlook.com>
2019-08-09 17:04:24 +0200

    Scheduling and dispatching tasks based on the info in the timetable

--- mrs/task_execution/dispatching/scheduler.py
+++ mrs/task_execution/dispatching/scheduler.py
@@ -7,5 +7,26 @@ class Scheduler(object):
         self.ccu_store = ccu_store
         self.stp = stp
 
-    def schedule(self, task):
-        pass
+        self.task_scheduled = False
+        self.navigation_start_time = -1  # of scheduled task
+
+        self.schedule = stp.get_stn()
+
+    def schedule_task(self, task, timetable):
+        print("Time table:", timetable.dispatchable_graph)
+
+        navigation_start = timetable.dispatchable_graph.get_task_navigation_start_time(task.id)
+        self.schedule.add_task(task, 1)
+        self.schedule.update_edge_weight(0, 1, navigation_start)
+        self.schedule.update_edge_weight(1, 0, -navigation_start)
+
+        self.task_scheduled = True
+
+        timetable.schedule = self.schedule
+        print("Schedule: ", timetable.schedule)
+        self.ccu_store.update_timetable(timetable)
+
+    def reset_schedule(self):
+        self.schedule = self.stp.get_stn()
+        self.task_scheduled = False
+
