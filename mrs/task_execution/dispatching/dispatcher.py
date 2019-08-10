cb1551d42853d5ef11ed5b4bda8f4ed4ef0ad3e7
Angela Enriquez <anenriqu@outlook.com>
2019-08-10 13:50:38 +0200

    Adding method to remove task from the timetable

--- mrs/task_execution/dispatching/dispatcher.py
+++ mrs/task_execution/dispatching/dispatcher.py
@@ -1,7 +1,10 @@
+import logging
+
 from ropod.utils.timestamp import TimeStamp as ts
 from mrs.task_execution.dispatching.scheduler import Scheduler
 from mrs.timetable import Timetable
 from stn.stp import STP
+from mrs.exceptions.task_allocation import NoSTPSolution
 
 
 class Dispatcher(object):
@@ -11,9 +14,15 @@ class Dispatcher(object):
         self.ccu_store = ccu_store
         self.task_cls = task_cls
         self.stp = STP(stp_solver)
+        self.stp_solver = stp_solver
         self.scheduler = Scheduler(robot_id, ccu_store, self.stp)
 
         self.timetable = self.get_timetable()
+
+        # corrective measure
+        # TODO: get this info from the config file
+        self.reschedule = True
+        self.reallocate = False
 
     def get_timetable(self):
         timetable_dict = self.ccu_store.get_timetable(self.id)
@@ -22,24 +31,60 @@ class Dispatcher(object):
         self.timetable = Timetable.from_dict(timetable_dict, self.stp)
         return self.timetable
 
+    # TODO: Add callback to receive
+
     def run(self):
+        self.get_timetable()
+        task = self.get_earliest_task()
+        if task:
+            self.check_earliest_task_status(task)
 
-        if not self.scheduler.task_scheduled and self.get_timetable():
-            task = self.get_next_task_to_schedule()
-            if task:
-                self.scheduler.schedule_task(task, self.timetable)
-                self.get_timetable()
+    def check_earliest_task_status(self, task):
+        # task is allocated
+        if task.status.status == 2:
+            self.schedule_task(task)
 
-        if self.scheduler.task_scheduled and self.time_to_dispatch():
-            self.dispatch()
+        # task is completed
+        elif task.status.status == 6:
+            if self.stp_solver == 'drea':
+                self.recompute_timetable(task)
             self.scheduler.reset_schedule()
 
-    def get_next_task_to_schedule(self):
+        # task is delayed and corrective measure reschedule is active
+        elif task.status.status == 5 and self.reschedule:
+            self.recompute_timetable(task)
+
+        elif task.status.status == 3 and self.time_to_dispatch():
+            self.dispatch()
+
+    def get_earliest_task(self):
         task_id = self.timetable.get_earliest_task_id()
         if task_id:
             task_dict = self.ccu_store.get_task(task_id)
             task = self.task_cls.from_dict(task_dict)
             return task
+
+    def schedule_task(self, task):
+        print("Scheduling task")
+        self.scheduler.schedule_task(task, self.timetable)
+        self.get_timetable()
+
+    def recompute_timetable(self, task):
+        try:
+            self.timetable.solve_stp()
+
+            logging.debug("Dispatchable graph %s: ", self.timetable.dispatchable_graph)
+            logging.debug("Robustness Metric %s: ", self.timetable.robustness_metric)
+
+        except NoSTPSolution:
+            logging.exception("The stp solver could not solve the problem")
+            self.update_task_status(task, 7)   # ABORTED
+            self.timetable.remove_task(task)
+
+    def update_task_status(self, task, status):
+        task.status.status = status
+        logging.debug("Updating task status to %s", task.status.status)
+        self.ccu_store.update_task(task)
 
     def time_to_dispatch(self):
         current_time = ts.get_time_stamp()
@@ -50,6 +95,7 @@ class Dispatcher(object):
     def dispatch(self):
         current_time = ts.get_time_stamp()
         print("Dispatching task at: ", current_time)
+
 
 
 
--- mrs/task_execution/dispatching/scheduler.py
+++ mrs/task_execution/dispatching/scheduler.py
@@ -1,3 +1,4 @@
+import logging
 
 
 class Scheduler(object):
@@ -7,7 +8,6 @@ class Scheduler(object):
         self.ccu_store = ccu_store
         self.stp = stp
 
-        self.task_scheduled = False
         self.navigation_start_time = -1  # of scheduled task
 
         self.schedule = stp.get_stn()
@@ -20,13 +20,17 @@ class Scheduler(object):
         self.schedule.update_edge_weight(0, 1, navigation_start)
         self.schedule.update_edge_weight(1, 0, -navigation_start)
 
-        self.task_scheduled = True
-
         timetable.schedule = self.schedule
         print("Schedule: ", timetable.schedule)
+
         self.ccu_store.update_timetable(timetable)
+        self.update_task_status(task, 3)  # 3 is SCHEDULED
+
+    def update_task_status(self, task, status):
+        task.status.status = status
+        logging.debug("Updating task status to %s", task.status.status)
+        self.ccu_store.update_task(task)
 
     def reset_schedule(self):
         self.schedule = self.stp.get_stn()
-        self.task_scheduled = False
 
