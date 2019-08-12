de6cfaee8d7842be1978b373468f245cb94be761
Angela Enriquez <anenriqu@outlook.com>
2019-08-12 10:31:10 +0200

    Adding corrective measure and freeze_window to the Dispatcher. A task is scheduled x seconds (defined by the freeze window) before its earliest navigation start

--- mrs/task_execution/dispatching/dispatcher.py
+++ mrs/task_execution/dispatching/dispatcher.py
@@ -9,32 +9,21 @@ from mrs.exceptions.task_allocation import NoSTPSolution
 
 class Dispatcher(object):
 
-    def __init__(self, robot_id, ccu_store, task_cls, stp_solver):
+    def __init__(self, robot_id, ccu_store, task_cls, stp_solver, corrective_measure, freeze_window):
         self.id = robot_id
         self.ccu_store = ccu_store
         self.task_cls = task_cls
         self.stp = STP(stp_solver)
         self.stp_solver = stp_solver
+        self.corrective_measure = corrective_measure
+        self.freeze_window = freeze_window
+
         self.scheduler = Scheduler(ccu_store, self.stp)
 
-        self.timetable = self.get_timetable()
-
-        # corrective measure
-        # TODO: get this info from the config file
-        self.reschedule = True
-        self.reallocate = False
-
-    def get_timetable(self):
-        timetable_dict = self.ccu_store.get_timetable(self.id)
-        if timetable_dict is None:
-            return
-        self.timetable = Timetable.from_dict(timetable_dict, self.stp)
-        return self.timetable
-
-    # TODO: Add callback to receive
+        self.timetable = Timetable.get_timetable(self.ccu_store, self.id, self.stp)
 
     def run(self):
-        self.get_timetable()
+        self.timetable = Timetable.get_timetable(self.ccu_store, self.id, self.stp)
         task = self.get_earliest_task()
         if task:
             self.check_earliest_task_status(task)
@@ -51,8 +40,12 @@ class Dispatcher(object):
             self.scheduler.reset_schedule(self.timetable)
 
         # task is delayed and corrective measure reschedule is active
-        elif task.status.status == 5 and self.reschedule:
+        elif task.status.status == 5 and self.corrective_measure == 're-schedule':
             self.recompute_timetable(task)
+
+        elif task.status.status == 5 and self.corrective_measure == 're-allocate':
+            self.scheduler.reset_schedule(self.timetable)
+            # TODO: Request re-allocation
 
         elif task.status.status == 3 and self.time_to_dispatch():
             self.dispatch()
@@ -67,7 +60,15 @@ class Dispatcher(object):
     def schedule_task(self, task):
         print("Scheduling task")
         self.scheduler.schedule_task(task, self.timetable)
-        self.get_timetable()
+        navigation_start = self.timetable.dispatchable_graph.get_task_navigation_start_time(task.id)
+        current_time = ts.get_time_stamp()
+
+        # Schedule the task freeze_window time before the navigation start and freeze it
+        #  i.e., the task cannot longer change position in the dispatchable graph
+        if (navigation_start - current_time) <= self.freeze_window:
+            self.scheduler.schedule_task(task, self.timetable)
+
+        self.timetable = Timetable.get_timetable(self.ccu_store, self.id, self.stp)
 
     def recompute_timetable(self, task):
         try:
