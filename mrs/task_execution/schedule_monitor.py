460ca09739cf2b1a907d1722270f8493d6483224
Angela Enriquez <anenriqu@outlook.com>
2019-08-20 18:00:57 +0200

    robot: robot, bidder and schedule_monitor inherit from RobotBase

--- mrs/task_execution/schedule_monitor.py
+++ mrs/task_execution/schedule_monitor.py
@@ -1,6 +1,7 @@
 from mrs.db_interface import DBInterface
+from mrs.robot_base import RobotBase
 
 
-class ScheduleMonitor(object):
-    def __init__(self, robot_common, schedule_monitor_config):
-        self.common = robot_common
+class ScheduleMonitor(RobotBase):
+    def __init__(self, robot_config, schedule_monitor_config):
+        super().__init__(**robot_config)
