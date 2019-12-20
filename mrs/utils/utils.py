59acf5673c0fd7e5de4614ef33a05c6616793e20
Ángela Enríquez Gómez <anenriqu@outlook.com>
2019-12-20 13:46:25 +0100

    utils: Add methods to load yaml files

---
+++ mrs/utils/utils.py
@@ -0,0 +1,18 @@
+from fmlib.utils.utils import load_file_from_module
+from fmlib.utils.utils import load_yaml
+import logging
+
+
+def load_yaml_file_from_module(module, file_name):
+    try:
+        file = load_file_from_module(module, file_name)
+        data = load_yaml(file)
+        return data
+    except FileNotFoundError as e:
+        logging.error(e)
+
+
+def load_yaml_file(file_name):
+    with open(file_name, 'r') as file_handle:
+        config = load_yaml(file_handle)
+    return config
