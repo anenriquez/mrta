5ee41d04c2c827965ccf1aa6bccba20a040f5c93
Ángela Enríquez Gómez <anenriqu@outlook.com>
2020-02-05 17:30:10 +0100

    setup: Add mrs.simulation module

--- setup.py
+++ setup.py
@@ -14,6 +14,7 @@ setup(name='mrs',
                 'mrs.timetable',
                 'mrs.dispatching',
                 'mrs.execution',
+                'mrs.simulation',
                 'mrs.utils'],
       version='0.2.0',
       description='Multi-Robot System (MRS) components for performing'
