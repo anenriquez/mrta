345ad13f4b37038842cb1e7db65d1838c2507efa
Ángela Enríquez Gómez <anenriqu@outlook.com>
2020-04-13 09:23:03 +0200

    results/plot: Do not show fig

--- experiments/results/plot.py
+++ experiments/results/plot.py
@@ -72,7 +72,7 @@ def plot_gantt(title, schedule, colors, group_tasks=False, borders=False, **kwar
     if index:
         title += '_' + str(index)
     fig.write_image(directory + '/%s.png' % title)
-    fig.show()
+    # fig.show()
 
 
 def get_gantt_robot_d_graph(timetable):
